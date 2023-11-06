91e7b09e54d6d0fea38a0792242dc096d2e7aede
pvk05 <philip.v.knudsen@gmail.com>
2023-11-06 09:08:43 +0100

    first netlify

---
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -0,0 +1,36 @@
+import { init } from '../serverless.js';
+
+export const handler = init((() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.9f969372.js","app":"_app/immutable/entry/app.dbb0ccd0.js","imports":["_app/immutable/entry/start.9f969372.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.0395f7aa.js","_app/immutable/entry/app.dbb0ccd0.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.a21d6cee.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('../server/nodes/0.js')),
+			__memo(() => import('../server/nodes/1.js')),
+			__memo(() => import('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})());
---
+++ .netlify/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.3469c622.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.a21d6cee.js","_app/immutable/chunks/singletons.0395f7aa.js"];
+export const stylesheets = [];
+export const fonts = [];
