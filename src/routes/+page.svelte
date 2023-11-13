4808da0f59273f544493af004b2d5e32ce23ed7d
pvk05 <philip.v.knudsen@gmail.com>
2023-11-13 08:53:08 +0100

    tested store reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    import { get } from 'svelte/store';
+    import { val } from './main.js'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-dfghjklødawjnkjndwakjnkjd HEY
+HEY
+
+<input type="number" name="testInp" id="testInp" bind:value={$val}>
+
+{$val}
